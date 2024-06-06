851b56bf978b74997031da29784bdf67a4b6062d
Mayra_Calderon <may2005_cc@hotmail.com>
2024-06-06 10:05:17 -0300

    Terminar el inicio de sesion

--- src/app/modules/atentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/atentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -8,8 +8,10 @@ import { Usuario } from 'src/app/models/usuario';
 })
 export class IniciosesionComponent {
   hide = true;
+  //la variable de sesionUsarios es una implementación de la interfaz Usuario
 
-  //importacion del modelo/interfaz de los datos necesarios
+
+  //importacion del modelo/interfaz de los datos, guardaría los datos que ingresasen
   usuarioIngresa: Usuario = {
     uid: '',
     nombre: '',
@@ -19,8 +21,9 @@ export class IniciosesionComponent {
     password: '',
   }
 
-  //coleccion de cuentas predeterminadas
-  sesionUsuarios: Usuario[] = [
+  //coleccion de cuentas predeterminadas locales
+
+  public sesionUsuarios: Usuario[] = [
     {
       uid: '',
       nombre: 'oreo',
@@ -44,16 +47,29 @@ export class IniciosesionComponent {
       email: 'donbizcocho@gmail.com',
       rol: 'visit',
       password: 'bizcochito000'
-    },
+    }
   ]
 
-  iniciarSesion(){
-    for (let i = 0; i < this.sesionUsuarios.length; i++) {
-      if ((this.usuarioIngresa.email === this.sesionUsuarios[i].email) && (this.usuarioIngresa.password === this.sesionUsuarios[i].password)) {
-        alert("Usted ha iniciado sesión")
-      } else {
-        alert("Los datos son incorrectos")
-      }
+
+  //en la función iniciar sesion tiene un evento de tipo onClick
+  iniciarSesion() {
+    const user = this.sesionUsuarios.find(u => u.email === this.usuarioIngresa.email && u.password === this.usuarioIngresa.password);
+    if (user) {
+      alert("Ha iniciado sesión de manera exitosa")
+    } else{
+      ("Los datos son incorrectos")
     }
+    //este for recorre el arreglo de la coleccion de usuarios ya predeterminada
+    /* for (let i = 0; i < this.sesionUsuarios.length; i++) {
+       //este condicional verifica que se encuentre una coincidencia en el objeto en el arreglo
+       if ((this.usuarioIngresa.email === this.sesionUsuarios[i].email) && (this.usuarioIngresa.password === this.sesionUsuarios[i].password)) {
+         alert("Usted ha iniciado sesión")
+         break
+       } else {
+         alert("Los datos son incorrectos")
+       }
+     }*/
+    /*Estoy tratando de resolver un problema que es el que sigue verificando el arreglo y da las tres vueltas por mas
+    que haya un break*/
   }
 }
